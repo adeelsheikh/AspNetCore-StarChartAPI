b145541ea63f86fb87c3fed6908f01a54465565d
Adeel Nayyer Sheikh <adeelnayyersheikh@gmail.com>
2018-12-15 05:09:41 +0400

    Controller: Post, Put, Patch and Delete actions.

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 using StarChart.Models;
@@ -16,6 +15,31 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]CelestialObject model)
+        {
+            _context.CelestialObjects.Add(model);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetById", new { id = model.Id }, model);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(x => x.Id == id || x.OrbitedObjectId == id);
+
+            if (celestialObjects.Any() == false)
+            {
+                return NotFound();
+            }
+
+            _context.CelestialObjects.RemoveRange(celestialObjects);
+            _context.SaveChanges();
+
+            return NoContent();
         }
 
         [HttpGet]
@@ -36,14 +60,14 @@ namespace StarChart.Controllers
         {
             var celestialObj = _context.CelestialObjects.Find(id);
 
-            if (celestialObj != null)
+            if (celestialObj == null)
             {
-                celestialObj.Satellites = GetSatellites(id);
-
-                return Ok(celestialObj);
+                return NotFound();
             }
 
-            return NotFound();
+            celestialObj.Satellites = GetSatellites(id);
+
+            return Ok(celestialObj);
         }
 
         [HttpGet("{name}")]
@@ -53,7 +77,7 @@ namespace StarChart.Controllers
 
             if (celestialObjects.Any())
             {
-                foreach(var celestialObject in celestialObjects)
+                foreach (var celestialObject in celestialObjects)
                 {
                     celestialObject.Satellites = GetSatellites(celestialObject.Id);
                 }
@@ -62,6 +86,42 @@ namespace StarChart.Controllers
             }
 
             return NotFound();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            var celestialObj = _context.CelestialObjects.Find(id);
+
+            if (celestialObj == null)
+            {
+                return NotFound();
+            }
+
+            celestialObj.Name = name;
+            _context.CelestialObjects.Update(celestialObj);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject model)
+        {
+            var celestialObj = _context.CelestialObjects.Find(id);
+
+            if (celestialObj == null)
+            {
+                return NotFound();
+            }
+
+            celestialObj.Name = model.Name;
+            celestialObj.OrbitalPeriod = model.OrbitalPeriod;
+            celestialObj.OrbitedObjectId = model.OrbitedObjectId;
+            _context.CelestialObjects.Update(celestialObj);
+            _context.SaveChanges();
+
+            return NoContent();
         }
 
         private List<CelestialObject> GetSatellites(int id)
